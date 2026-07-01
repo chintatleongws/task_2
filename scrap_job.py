bee4a1d7c0eb19a23148a28ca4b21b9f7c9f31ee
Chin Tat Leong <chin.tatleong@ws.mcsaatchi.com>
2026-07-01 12:30:49 +0100

    feat: added BS to parse text to HTML and dumped in file as HTML

--- scrap_job.py
+++ scrap_job.py
@@ -5,4 +5,10 @@ from bs4 import BeautifulSoup
 # stand in url, can swap a real one
 url = "https://www.geeksforgeeks.org/dsa/dsa-tutorial-learn-data-structures-and-algorithms/"
 response = requests.get(url)
-print(response.text)
+
+# Add in bs4
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.prettify())  # prints well-formatted HTML
+
+with open("sample_parsed_page.html", "w", encoding="utf-8") as f:
+    f.write(soup.prettify())
