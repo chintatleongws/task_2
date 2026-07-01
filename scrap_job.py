82c2d8fcbbff3a857a56c77894e77ed583595bf8
Chin Tat Leong <chin.tatleong@ws.mcsaatchi.com>
2026-07-01 14:54:53 +0100

    feat: web scrap with list of url and names. Raw html store in bronze bucket

--- scrap_job.py
+++ scrap_job.py
@@ -2,13 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 
 
-# stand in url, can swap a real one
-url = "https://www.geeksforgeeks.org/dsa/dsa-tutorial-learn-data-structures-and-algorithms/"
-response = requests.get(url)
+url_list = [
+    "https://www.geeksforgeeks.org/dsa/dsa-tutorial-learn-data-structures-and-algorithms/",
+    "https://airflow.apache.org/docs/apache-airflow/stable/index.html"
+]
+url_names = [
+    "DSA Tutorial",
+    "Airflow Documentation"
+]
 
-# Add in bs4
-soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.prettify())  # prints well-formatted HTML
+for i, url in enumerate(url_list):
+    # stand in url, can swap a real one
+    response = requests.get(url)
 
-with open("sample_parsed_page.html", "w", encoding="utf-8") as f:
-    f.write(soup.prettify())
+    # Add in bs4
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print(soup.prettify())  # prints well-formatted HTML
+    with open(f"./data/bronze/raw_html/{url_names[i]}.html", "w", encoding="utf-8") as f:
+        f.write(soup.prettify())
